44c54f69b8e3e95aa53ed080c08f2787fff4f495
Онищенко Арина <onishenkoarinka@yandex.ru>
2024-05-31 03:34:08 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -2,5 +2,8 @@ function showTime(){
     let today = new Date();
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
+
+    time = document.querySelector('.str')
+    time.style.transform = `rotate(${new Date().getSeconds() * 6}deg) translate(0, -75px)`; 
 }
 setInterval(showTime,1000);
